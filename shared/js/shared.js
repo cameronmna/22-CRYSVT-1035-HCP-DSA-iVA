11e5eac45fad4cf8c945a57844e98bb00e5a2ebe
cameron cashwell <cameron.cashwell@mna.co>
2023-02-21 18:17:41 -0800

    latest

--- shared/js/shared.js
+++ shared/js/shared.js
@@ -3,31 +3,18 @@ window.addEventListener('load', function() {
   // Define the function that creates and shows the modal
   function showModal(element) {
     console.log(element);
+
+    let actualID = element.nextElementSibling.id;
+    console.log(actualID);
     // Get the content of the element with the same ID as the clicked element
-    const content = document.getElementById(element.id + '-content').innerHTML;
+    const content = document.getElementById(actualID);
+    content.style.display = 'block';
 
-    // Create the modal container
-    const modalContainer = document.createElement('div');
-    modalContainer.className = 'modal-container';
+    // Add an event listener to all elements with the "modal-trigger" class
+    const modalContainer = document.getElementsByClassName('modal-close');
+    for (let i = 0; i < modalContainer.length; i++) {
 
-    // Create the modal content
-    const modalContent = document.createElement('div');
-    modalContent.className = 'modal-content';
-    modalContent.innerHTML = content;
-
-    // Add the content to the container and add the container to the page
-    modalContainer.appendChild(modalContent);
-    document.body.appendChild(modalContainer);
-
-    // Create a close button
-    const closeButton = document.createElement('button');
-    closeButton.className = 'modal-close';
-    closeButton.innerHTML = '&times;';
-    modalContent.appendChild(closeButton);
-
-    // Center the modal vertically and horizontally
-    modalContent.style.top = `${(window.innerHeight - modalContent.offsetHeight) / 2}px`;
-    modalContent.style.left = `${(window.innerWidth - modalContent.offsetWidth) / 2}px`;
+    }
 
     // Close the modal when the user clicks the close button or outside of it
     function closeModal() {
