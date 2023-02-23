8c9e9d740e65f824d35a233584536013a72c655e
cameron cashwell <cameron.cashwell@mna.co>
2023-02-23 10:19:13 -0800

    latest

--- shared/js/shared.js
+++ shared/js/shared.js
@@ -1,37 +1,20 @@
 window.addEventListener('load', function() {
 
-  // Define the function that creates and shows the modal
-  function showModal(element) {
-    console.log(element);
+  var modals = document.querySelectorAll(".modal");
+  var buttons = document.querySelectorAll(".modal-button");
+  var closeButtons = document.querySelectorAll(".close");
 
-    let actualID = element.nextElementSibling.id;
-    console.log(actualID);
-    // Get the content of the element with the same ID as the clicked element
-    const content = document.getElementById(actualID);
-    content.style.display = 'block';
-
-    // Add an event listener to all elements with the "modal-trigger" class
-    const modalContainer = document.getElementsByClassName('modal-close');
-    for (let i = 0; i < modalContainer.length; i++) {
-
-    }
-
-    // Close the modal when the user clicks the close button or outside of it
-    function closeModal() {
-      document.body.removeChild(modalContainer);
-    }
-    modalContainer.addEventListener('click', (event) => {
-      if (event.target === modalContainer) {
-        closeModal();
-      }
+  buttons.forEach(function(button, index) {
+    button.addEventListener("click", function() {
+      modals[index].style.display = "block";
     });
-    closeButton.addEventListener('click', closeModal);
-  }
+  });
 
-  // Add an event listener to all elements with the "modal-trigger" class
-  const modalTriggers = document.getElementsByClassName('modal-trigger');
-  for (let i = 0; i < modalTriggers.length; i++) {
-    const trigger = modalTriggers[i];
-    trigger.addEventListener('click', () => showModal(trigger));
-  }
+  closeButtons.forEach(function(closeButton) {
+    closeButton.addEventListener("click", function() {
+      modals.forEach(function(modal) {
+        modal.style.display = "none";
+      });
+    });
+  });
 });
